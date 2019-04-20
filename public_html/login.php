242898fd89c682bf2b821ad387b30c9c2ca4db84
steve-kent <39534870+steve-kent@users.noreply.github.com>
2019-04-20 14:56:41 -0400

    Set login form to not show if already logged in

--- public_html/login.php
+++ public_html/login.php
@@ -1,5 +1,6 @@
 <?php
 require("page.php");
+require('staticStuff.php');
 // Create new page
 $page = new Page();
 session_start();
@@ -24,19 +25,32 @@ if (isset($_SESSION['loginError']))
     unset($_SESSION['loginError']);
 }
 
+if(isset($_SESSION['validUser']))
+{
+    $user = $_SESSION['validUser'];
+        // Add content
+    $content = "
+    <div id='container' >
+    <h3 id='loginTitle'>You're already logged in as <span class='i'>$user<span></h3>
+    $manageUserHeader</div>";
+}
+else
+{
 // Add content
 $content = <<< EOT
-<div id="container" ><div class="centerStuff">
-$error
-<form id="login" name="login" method="post" action="validateLogin.php" onsubmit="return validateForm(this)">
-<p id="validNameUser" class="invalidMsg">You must enter a username and password.</p>
-Username: <input type="text" name="userName" id="userName" size="20" tabindex="1" accesskey="u" autofocus> <br> 
-password: <input type="password" name="password" id="password" size="20" tabindex="2" accesskey="p"> <br> 
-<input type=submit name="login" value="Login">
-</form>
-</div></div>
+    <div id="container" ><div class="centerStuff"><div class="formContainer">
+    <div class="centerStuff"><h1 id="loginTitle">DDGC Login</h1>
+    $error
+    </div>
+    <form id="login" name="login" method="post" action="validateLogin.php" onsubmit="return validateForm(this)">
+    <p id="validNameUser" class="invalidMsg">You must enter a username and password.</p>
+    Username: <input type="text" name="userName" id="userName" size="20" tabindex="1" accesskey="u" autofocus> <br> 
+    password: <input type="password" name="password" id="password" size="20" tabindex="2" accesskey="p"> <br> 
+    <input type=submit name="login" value="Login">
+    </form>
+    </div></div></div>
 EOT;
-
+}
 $page->content = $content;
 // Display the page
 $page->Display();
--- public_html/staticStuff.php
+++ public_html/staticStuff.php
@@ -10,5 +10,7 @@ $manageUserHeader = "
 </div>
 </div>";
 
+
+
 ?>
 
