f8ef79fc546aa786a1f9485900a47df7233e36c3
steve-kent <39534870+steve-kent@users.noreply.github.com>
2019-04-21 16:27:49 -0400

    Refactor page to not be object/class

--- public_html/page.php
+++ public_html/page.php
@@ -1,134 +1,104 @@
 <?php
-
-class Page
+// Creates the elements in the <head> of the page
+function WriteHead($title = "DeBary Disc Golf Club", $desc = "DeBary Disc Golf Club", $shouldIndex = 1)
 {
-    private $content = "";
-    private $desc = "DeBary Disc Golf Club";
-    private $title = "DeBary Disc Golf Club";
-    private $navButtons = array(
+    ?>
+    <!doctype html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="description" content="<?=$desc?>">
+    <meta name="robots" content="<?=$shouldIndex ? 'index' : 'noindex' ?>">
+    <title><?=$title?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Righteous" rel="stylesheet">
+    <link rel="icon" href="images/logo_200.png">
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113170186-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-113170186-1');
+    </script>
+    </head>
+    <?php
+}
+
+// Write the header section/nav of the page
+function WriteHeader()
+{
+    ?>
+        <header><a href="index.php"><img src="images/logo_200.png" alt="DeBary Disc Golf Club Logo" ></a>
+	    <div id="mobile_header_breaks">
+	    <br><br><br>
+	    </div>
+	    <h1>DeBary Disc Golf Club</h1>
+        <?php AddNav(); ?>
+	    </header>
+    <?php
+}
+
+// Adds the nav buttons
+function AddNav()
+{
+    $navButtons = array(
         "Members" => "members.php",
         "Courses" => "courses.php",
         "Calendar" => "calendar.php",
         "Links" => "links.php"
     );
-    private $headAdditions = "";
-    private $styleSheet = "css/styles.css";
-    private $shouldIndex = 1;
-    // Setter for private variables
-    public function __set($name, $val)
+    ?>
+    <div class="nav">
+	    	<ul>
+            <?php
+                while (list($name, $url) = each($navButtons))
+                {
+                    AddNavButton($name, $url);
+                }
+            ?>		
+	    	</ul>
+	    </div>
+    <?php
+}
+
+// Adds a nav button
+function AddNavButton($name, $url)
+{
+    echo "<li ";
+    if (isCurrentUrl($url))
     {
-        $this->$name = $val;
+        echo "class=''current' ";
     }
+    echo '><a href="'.$url.'">'.$name."</a></li>";
+}
 
-    // Creates the elements in the <head> of the page
-    private function WriteHead()
+// Returns true if $url is the current page
+function isCurrentUrl($url)
+{
+    if(strpos($_SERVER['PHP_SELF'], $url) === false)
     {
-        ?>
-        <!doctype html>
-        <html lang="en">
-        <head>
-        <meta charset="utf-8">
-        <meta name="description" content="<?=$this->desc?>">
-        <meta name="robots" content="<?=$this->shouldIndex ? 'index' : 'noindex' ?>">
-        <title><?=$this->title?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="<?=$this->styleSheet?>">
-        <link href="https://fonts.googleapis.com/css?family=Comfortaa|Righteous" rel="stylesheet">
-        <link rel="icon" href="images/logo_200.png">
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113170186-1"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'UA-113170186-1');
-        </script>
-        <?=$this->headAdditions?>
-        </head>
-        <?php
+        return false;
     }
-
-    // Write the header section/nav of the page
-    private function AddHeader()
+    else
     {
-        ?>
-            <header><a href="index.php"><img src="images/logo_200.png" alt="DeBary Disc Golf Club Logo" ></a>
-    	    <div id="mobile_header_breaks">
-    	    <br><br><br>
-    	    </div>
-    	    <h1>DeBary Disc Golf Club</h1>
-            <?php $this -> AddNav(); ?>
-    	    </header>
-        <?php
-    }
-
-    // Adds the nav buttons
-    private function AddNav()
-    {
-        ?>
-        <div class="nav">
-    	    	<ul>
-                <?php
-                    while (list($name, $url) = each($this->navButtons))
-                    {
-                        $this -> AddNavButton($name, $url);
-                    }
-                ?>		
-    	    	</ul>
-    	    </div>
-        <?php
-    }
-
-    // Adds a nav button
-    private function AddNavButton($name, $url)
-    {
-        echo "<li ";
-        if ($this->isCurrentUrl($url))
-        {
-            echo "class=\"current\" ";
-        }
-        echo '><a href="'.$url.'">'.$name."</a></li>";
-
-    }
-
-    // Returns true if $url is the current page
-    private function isCurrentUrl($url)
-    {
-        if(strpos($_SERVER['PHP_SELF'], $url) === false)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
-    private function AddFooter()
-    {
-        ?>
-        <footer>
-	    <p>
-	    <a href="https://www.facebook.com/groups/192035720947651/"><img src="images/FB.png" alt="FaceBook Logo"></a> 
-	    <span class="nowrap">Club Information: <a href="mailto:brianroberts2526@yahoo.com" >Brian Roberts</a></span> | 
-	    <span class="nowrap">Webmaster: <a href="mailto:stevekentsphone@gmail.com" >Steve Kent</a></span> <br>
-        &copy; Copyright 2019 DeBary Disc Golf Club</p>
-        
-	    </footer>
-        
-        <?php
-    }
-
-    // diplay the page
-    public function Display()
-    {
-        $this -> WriteHead();
-        $this -> AddHeader();
-        echo $this -> content;
-        $this -> AddFooter();
-
-
+        return true;
     }
 }
 
+function AddFooter()
+{
+    ?>
+    <footer>
+ <p>
+ <a href="https://www.facebook.com/groups/192035720947651/"><img src="images/FB.png" alt="FaceBook Logo"></a> 
+ <span class="nowrap">Club Information: <a href="mailto:brianroberts2526@yahoo.com" >Brian Roberts</a></span> | 
+ <span class="nowrap">Webmaster: <a href="mailto:stevekentsphone@gmail.com" >Steve Kent</a></span> <br>
+    &copy; Copyright 2019 DeBary Disc Golf Club</p>
+    
+ </footer>
+</body>
+</html>
+    <?php
+}
 ?>
--- public_html/staticStuff.php
+++ public_html/staticStuff.php
@@ -1,14 +1,18 @@
 <?php
 
-$manageUserHeader = "
-<div id='manageContainer'>
-<div id='manageNav'>
-<div class='button manageButtons'><a href='players.php'>Players List</a></div>
-<div class='button manageButtons'><a href='add_player.php'>Add Player</a></div>
-<div class='button manageButtons'><a href='score_round.php'>Enter Scores</a></div>
-<div class='button manageButtons'><a href='logout.php'>Log out</a></div>
-</div>
-</div>";
+function WriteManageUserHeader()
+{
+    ?>
+    <div id='manageContainer'>
+    <div id='manageNav'>
+    <div class='button manageButtons'><a href='players.php'>Players List</a></div>
+    <div class='button manageButtons'><a href='add_player.php'>Add Player</a></div>
+    <div class='button manageButtons'><a href='score_round.php'>Enter Scores</a></div>
+    <div class='button manageButtons'><a href='logout.php'>Log out</a></div>
+    </div>
+    </div>
+    <?php
+}
 
 
 
