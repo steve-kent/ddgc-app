6d05ed00b03550696bb40e36b6b6b2618c307e6a
steve-kent <39534870+steve-kent@users.noreply.github.com>
2020-02-15 16:59:13 -0500

    Added link to Clam Jam on home page

--- public_html/index.php
+++ public_html/index.php
@@ -23,7 +23,7 @@ WriteHeader();
         <a href="war.php">War 2020 Qualifier Layouts</a>
     </div>
     <div class="button30 button margin10">
-        <a href="raffle.php">Pattie Fundraiser Raffle</a>
+        <a href="https://www.discgolfscene.com/tournaments/The_Clam_Jam_Benefiting_The_Surfrider_Foundation_s_Florida_Chapters_2020">The Clam Jam for Charity</a>
     </div>
     <p>The DeBary Disc Golf Club is a not-for-profit organization whose mission is to foster, promote, and improve the sport of disc golf. 
 As well as represent the City of DeBary through charitable events and conservation of natural habitats. Finally, to fellowship among 
